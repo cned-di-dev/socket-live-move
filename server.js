eed183ee03bbf84af23f92a6ae82005b913a1643
Enguerran Ws <enguerranweiss@gmail.com>
2015-12-21 10:48:07 +0100

    desktop > mobile bind

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var http = require('http'),
     io = require('socket.io').listen(server),
     pjson = require('./package.json'),
     publicDir =  __dirname + '/public',
-    port = process.env.PORT || 5000;
+    port = process.env.PORT || 5000,
+    connexions = [];
 
 function generateId()
 {
@@ -16,29 +17,39 @@ function generateId()
     return text;
 }
 app.use(express.static('public'));
+
 // Server conf / routes
 // app.get("/", function (req, res) {
 //   console.log('Request on : /');
 //   res.sendFile(publicDir+"/index.html");
 //   res.end();
 // });
+
 // Server launch
 server.listen(port, function () {
 	console.info(pjson.name +' is running on 127.0.0.1:'+port+' :)');
-
 });
 
 
 // Socket EVENTS
 io.on('connection', function(socket){
+  var newID;
+  if(socket.handshake.query.id){
+    newID = socket.handshake.query.player;
+  }
+  else {
+    newID = generateId();
+  }
 
+  io.emit('setID', newID);
+  socket.join(newID);
+  socket.on('disconnect', function() {
+    socket.leave(newID);
+  });
   socket.on('sendMove', function(aigVal){
-      console.log(aigVal);
     	aigVal.aigX = aigVal.aigX;
     	aigVal.aigY = aigVal.aigY;
     	aigVal.aigZ = aigVal.aigZ * 15;
-    	io.emit('getMove', aigVal);
-
+    	io.to(newID).emit('getMove', aigVal);
   });
-
 });
